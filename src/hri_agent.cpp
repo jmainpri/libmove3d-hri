8b3230338d0c2e68f53e7ef5c52e9a305cf8035f
Matthieu Warnier <mwarnier@laas.fr>
2011-06-21 12:43:58 +0200

    change foa and point_tolerance in hri_agent to fit severin needs.

--- src/hri_agent.cpp
+++ src/hri_agent.cpp
@@ -324,20 +324,20 @@ HRI_PERSP * hri_create_agent_perspective(HRI_AGENT * agent, p3d_env *env)
     case HRI_ACHILE:
       persp->camjoint = agent->robotPt->joints[42];
       persp->fov = 160;
-      persp->foa = 70; // TODO: By default This should be 30. Change for a particular manip while waiting a fix on vis placements
+      persp->foa = 40; //70; // TODO: By default This should be 30. Change for a particular manip while waiting a fix on vis placements
       persp->tilt_jnt_idx = 6;
       persp->pan_jnt_idx  = 5;
       persp->pointjoint = agent->robotPt->joints[36];
-      persp->point_tolerance = 20;
+      persp->point_tolerance = 30; //20;
     break;
     case HRI_HERAKLES:
       persp->camjoint = agent->robotPt->joints[46];
       persp->fov = 160;
-      persp->foa = 70; // TODO: By default This should be 30. Change for a particular manip while waiting a fix on vis placements
+      persp->foa = 40; //70; // TODO: By default This should be 30. Change for a particular manip while waiting a fix on vis placements
       persp->tilt_jnt_idx = 6;
       persp->pan_jnt_idx  = 5;
       persp->pointjoint = agent->robotPt->joints[40];
-      persp->point_tolerance = 20;
+      persp->point_tolerance = 30; //20;
       break;
     case HRI_SUPERMAN:
       persp->camjoint = agent->robotPt->joints[1]; //TODO: put the correct value
